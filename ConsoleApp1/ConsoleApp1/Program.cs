a41c6f631951dd9099d9e0e1481754d4eb5fd29f
LAPTOP-1H4J36OP\Fedor Andreevich <7934650@gmail.com>
2021-08-29 15:21:15 +0300

    Init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a = "vert";
+            //int b = 5;
+            //double c = 5.6;
+            //char d = '$';
+            //bool e = true;
+            //bool r = false;
+            //Console.WriteLine($"{a}");
+            //string name;
+            //int age;
+            //string country;
+            //string city;
+            //bool student;
+            //char classNumber;
+            //Console.WriteLine("Как тебя зовут, педрильник?");
+            //name = Console.ReadLine();
+            //Console.WriteLine("На сколько ты стар?");
+            //age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Из какой ты страны?");
+            //country = Console.ReadLine();
+            //Console.WriteLine("Ты пездюк ученик?");
+            //double r;
+            //double s,p;
+            //Console.Write("Сторона крвадрата?");
+            //r = Convert.ToDouble(Console.ReadLine());
+            //s = r * r;
+            //p = r * 4;
+            //Console.WriteLine($"Площадь квадрата = {s}");
+            //Console.WriteLine($"Периметр квадрата = {p}");
+            //Console.WriteLine("Ты пидор!!!!!");
+            //int dayOfWeek = 0;
+            //Console.WriteLine("Введи число от 1 до 7");
+            //dayOfWeek = Convert.ToInt32(Console.ReadLine());
+            //if (dayOfWeek == 1)
+            //{
+            //    Console.WriteLine("твой день понедельник");
+            //}
+            //else if (dayOfWeek == 2)
+            //{ Console.WriteLine("Твой день вторник"); 
+            //}
+            //else if (dayOfWeek == 3)
+            //{
+            //    Console.WriteLine("Твой день среда");
+            //}
+            //else if (dayOfWeek == 4)
+            //{
+            //    Console.WriteLine("Твой день четверг");
+            //}
+            //else if (dayOfWeek == 5)
+            //{
+            //    Console.WriteLine("Твой день пятница");
+            //}
+            //else if (dayOfWeek == 6)
+            //{
+            //    Console.WriteLine("Твой день суббота");
+            //}
+            //else if (dayOfWeek == 7)
+            //{
+            //    Console.WriteLine("Твой день воскресенье");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Перечитай правила, от 1 до 7");
+            //}int
+            int dayOfWeek = 0;
+            string exit = "yes";
+            while (exit == "yes" || exit == "Yes" || exit == "No"|| exit == "no")
+            {
+
+                Console.WriteLine("Введи число от 1 до 7");
+                dayOfWeek = Convert.ToInt32(Console.ReadLine());
+                if (dayOfWeek == 1)
+                {
+                    Console.WriteLine("твой день понедельник");
+                }
+                else if (dayOfWeek == 2)
+                {
+                    Console.WriteLine("Твой день вторник");
+                }
+                else if (dayOfWeek == 3)
+                {
+                    Console.WriteLine("Твой день среда");
+                }
+                else if (dayOfWeek == 4)
+                {
+                    Console.WriteLine("Твой день четверг");
+                }
+                else if (dayOfWeek == 5)
+                {
+                    Console.WriteLine("Твой день пятница");
+                }
+                else if (dayOfWeek == 6)
+                {
+                    Console.WriteLine("Твой день суббота");
+                }
+                else if (dayOfWeek == 7)
+                {
+                    Console.WriteLine("Твой день воскресенье");
+                }
+                else
+                {
+                    Console.WriteLine("Перечитай правила, от 1 до 7");
+                }
+
+                Console.WriteLine("Хотите продолжить?Для продолжения введите yes, для выхода любой другой символ.");
+                exit = Console.ReadLine();
+            }
+        }
+    }
+}
